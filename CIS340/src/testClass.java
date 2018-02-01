5358a435b6eb1499919c2d10941b65d5225e2f83
HenryLiu <HenryLiu@home>
2018-02-01 13:38:34 -0700

    qui2

---
+++ CIS340/src/testClass.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double perCredit = 999.99;
+		String courseName = "Java";
+		String courseType = "600 Level ";
+		
+		if (courseName == "Java") {
+			if (perCredit < 1000) {
+				if (courseType =="600 Level") {
+					System.out.println("600, within");
+				}else {
+					System.out.println("within");
+				}
+			}else {
+				System.out.println("expensive");
+			}
+		}
+		
+	}
+
+}

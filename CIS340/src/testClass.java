7ade5e7c3586f3ddfa5a4342cda1c3871c6eb55d
henryliu <henryliu@COBD10FH.Busdom.ColoState.EDU>
2018-02-08 15:13:44 -0700

    passing value through method

--- CIS340/src/testClass.java
+++ CIS340/src/testClass.java
@@ -1,8 +1,28 @@
-
+import javax.swing.*;
 public class testClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		int a = 1, b=2;
+		int [] c = {1,2};
+		
+		System.out.println("a="+a+";b="+b);
+		System.out.println("a[]="+c[0]+", "+c[1]);
+		
+		swap (a,b);
+		swapFirst (c);
+		
+		System.out.println("a="+a+";b="+b);
+		System.out.println("a[]="+c[0]+", "+c[1]);
+		
+		
+		
+		
+		//System.out.print((int)'A');
+		
+		/*
 		double perCredit = 999.99;
 		String courseName = "Java";
 		String courseType = "600 Level ";
@@ -18,7 +38,19 @@ public class testClass {
 				System.out.println("expensive");
 			}
 		}
-		
+		*/
+	}
+	public static void swap(int n1, int n2) {
+		int temp = n1;
+		n1=n2;
+		n2=temp;
+		return;
+	}
+	public static void swapFirst(int[] array){
+		int temp = array[0];
+		array[0] = array [1];
+		array[1] = temp;
+		return;
 	}
 
 }

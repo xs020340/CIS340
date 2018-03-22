104e3fb8969b6cc6ddb605626b791fd693ec0f54
henry <henry@DESKTOP-LCSUCTB>
2018-03-22 14:59:34 -0600

    ICE5

---
+++ CIS340/src/Henry_ICE5/Customer.java
@@ -0,0 +1,68 @@
+package Henry_ICE5;
+
+public class Customer {
+	
+	private String custName;
+	private int custNumber;
+	private int custRate;
+	private Waterbill bill;
+	private static int numOfCustomers;
+	
+	public Customer() {
+		this.custName=null;
+		this.custNumber=0;
+		this.bill=null;
+		numOfCustomers=0;
+	}
+	
+	public Customer(String custName, int custNumber, Waterbill bill) {
+		super();
+		this.custName = custName;
+		this.custNumber = custNumber;
+		this.bill = bill;
+		numOfCustomers++;
+	}
+
+	public String getCustName() {
+		return custName;
+	}
+
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+
+	public int getCustNumber() {
+		return custNumber;
+	}
+
+	public void setCustNumber(int custNumber) {
+		this.custNumber = custNumber;
+	}
+
+	public Waterbill getBill() {
+		return bill;
+	}
+
+	public void setBill(Waterbill bill) {
+		this.bill = bill;
+	}
+
+	public static int getNumOfCustomers() {
+		return numOfCustomers;
+	}
+
+	public static void setNumOfCustomers(int numOfCustomers) {
+		Customer.numOfCustomers = numOfCustomers;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [custName=" + custName + ", custNumber=" + custNumber + ", custRate=" + custRate + ", bill="
+				+ bill.toString() + "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ CIS340/src/Henry_ICE5/Henry_ICE5.java
@@ -0,0 +1,120 @@
+package Henry_ICE5;
+
+import javax.swing.JOptionPane;
+import java.io.*;
+
+public class Henry_ICE5 {
+	
+	static Customer[] customers;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//declare variables
+		int gallons=0;
+		String custName=null;
+		int custNumber=0;
+		int custType=0;
+		int noOfCustomers=0;
+		
+		//obtain user input-number of customers
+		try {
+			noOfCustomers=Integer.parseInt(JOptionPane.showInputDialog(null, "Enter number of customers: "));
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, "Invalid Value!" + "\nEnter a valid value for number of customers!");
+			System.exit(0);
+		}
+		
+		//initialize array with size
+		customers=new Customer[noOfCustomers];
+		
+		for(int i=0;i<noOfCustomers;i++){
+			//start for-loop
+		
+			//input customer type
+			try{
+			custType=Integer.parseInt(JOptionPane.showInputDialog(null, "Enter Customer Type \n1-Single Family \n2-Duplex"));
+			if (custType!=1 && custType!=2)
+				throw new Exception(); //control goes to catch clause
+			}
+			catch (Exception ex) {
+				JOptionPane.showMessageDialog(null,  "Invalid Customer Rate! \nIt must be 1 or 2");
+				System.exit(0);
+			}
+			
+			//obtain user number
+			try {
+			custNumber = Integer.parseInt(JOptionPane.showInputDialog(null,
+					"Enter customer number: "));
+			if (String.valueOf(custNumber).length()!=6)
+				throw new Exception(); //control goes to catch clause
+			} catch (Exception ex) {
+				JOptionPane.showMessageDialog(null, "Please enter a valid 6-digit customer number!"
+						,"ERROR MESSAGE",JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			}
+			
+			//input customer name
+			custName=JOptionPane.showInputDialog(null, "Enter customer name: ");
+			if (custName.isEmpty()){
+				JOptionPane.showMessageDialog(null,  "Please enter a valid customer name!\nIt cannot be empty!");
+				System.exit(0);
+			}
+			
+			//input number of gallons
+			try {
+				gallons=Integer.parseInt(JOptionPane.showInputDialog(null, "enter number of gallons: "));
+				if (gallons <0){
+					throw new Exception(); //go to catch clause
+				}
+			}catch (Exception ex){
+				JOptionPane.showMessageDialog(null, "Please enter a valid value for gallons!\nIt cannot be negative!", "ERROR_MESSAGE"
+						,JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			}
+			addCustomer(custType, custNumber, custName, gallons);
+			
+		} // end for-loop
+		
+		displayBill();
+		try {
+			writeToFile();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	} //end main
+
+	public static void addCustomer (int custType, int custNumber, String custName, int gallons) {
+		
+		Waterbill bill = new Waterbill(gallons, custType);
+		customers[Customer.getNumOfCustomers()]= new Customer(custName, custNumber, bill);
+		
+	} //end addCustomer
+	
+	public static void displayBill() {
+		String output="";
+		for (int i=0; i<customers.length; i++) {
+			output+=customers[i].toString()+"\n";
+		}
+		JOptionPane.showMessageDialog(null, output, "WATERBILL", JOptionPane.INFORMATION_MESSAGE);
+	} //end displayBill()
+	
+	public static void writeToFile() throws FileNotFoundException {
+		String fileOutput="";
+		File myfile=new File("billdetails.txt");
+		
+		PrintWriter outputFile=new PrintWriter(myfile);
+		
+		for (int i=0; i<customers.length; i++) {
+			fileOutput+=customers[i].toString()+"\n";
+		}
+		
+		outputFile.print(fileOutput);
+		outputFile.close();
+		
+	}
+
+}
